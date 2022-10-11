0e80a659b5f1e168754393c7f5010eb25a3c5683
adkelley <adkelley@mac.com>
2022-10-11 06:38:49 -0700

    Before refactor to return a Server without awaiting it 3.5

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,8 +1,23 @@
 //! tests/health_check.rs
 
-use zero2prod::main;
+#[actix_web::test]
+async fn health_check_works() {
+    // Arrange
+    spawn_app().await.expect("Failed our spawn app.");
+    let client = reqwest::Client::new();
 
-#[test]
-fn dummy_test() {
-    main()
+    // Act
+    let repsonse = client
+        .get("http://127.0.0.1:8000/health_check")
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    // Assert
+    assert!(repsonse.status().is_success());
+    assert_eq!(Some(0), repsonse.content_length());
+}
+
+async fn spawn_app() -> std::io::Result<()> {
+    zero2prod::run().await
 }
