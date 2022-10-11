c168200ee8ae9afa7efcb6a43aab07214b126acb
adkelley <adkelley@mac.com>
2022-10-11 06:59:23 -0700

    Refactored to return a Server without awaiting 3.5

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -3,21 +3,22 @@
 #[actix_web::test]
 async fn health_check_works() {
     // Arrange
-    spawn_app().await.expect("Failed our spawn app.");
+    spawn_app();
     let client = reqwest::Client::new();
 
     // Act
-    let repsonse = client
+    let response = client
         .get("http://127.0.0.1:8000/health_check")
         .send()
         .await
         .expect("Failed to execute request.");
 
     // Assert
-    assert!(repsonse.status().is_success());
-    assert_eq!(Some(0), repsonse.content_length());
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
 }
 
-async fn spawn_app() -> std::io::Result<()> {
-    zero2prod::run().await
+fn spawn_app() {
+    let server = zero2prod::run().expect("Failed to bind address");
+    let _ = tokio::spawn(server);
 }
