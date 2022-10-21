2284fb7465328c88ec234954ec6fb6a2bf3058df
adkelley <adkelley@mac.com>
2022-10-21 06:39:41 -0700

    Added and ran first migration 3.8.4.2.4

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -3,7 +3,8 @@ set -x
 set -eo pipefail
 
 if ! [ -x "$(command -v psql)" ]; then
-    echo >&2 "Error: psql is not installed." exit 1
+    echo >&2 "Error: psql is not installed." 
+    exit 1
 fi
 
 if ! [ -x "$(command -v sqlx)" ]; then
@@ -23,15 +24,18 @@ DB_NAME="${POSTGRES_DB:=newsletter}"
 # Check if a custom port has been set, otherwise default to '5432'
 DB_PORT="${POSTGRES_PORT:=5432}"
 
-# Launch postgres using Docker
-docker run \
-    -e POSTGRES_USER=${DB_USER} \
-    -e POSTGRES_PASSWORD=${DB_PASSWORD} \
-    -e POSTGRES_DB=${DB_NAME} \
-    -p ${POSTGRES_PORT}:5432 \
-    -d postgres:14.5 \
-    postgres -N 1000
+# Allow to skip Docker if a dockerized Postgres database is already running
+if [[ -z "${SKIP_DOCKER}" ]]
+then
+    docker run \
+	-e POSTGRES_USER=${DB_USER} \
+	-e POSTGRES_PASSWORD=${DB_PASSWORD} \
+	-e POSTGRES_DB=${DB_NAME} \
+	-p ${POSTGRES_PORT}:5432 \
+	-d postgres:14.5 \
+	postgres -N 1000
 # ^ Increased maximum number of connections for testing purposes
+fi
 
 # Keep pinging Postgres until it's ready to accept commands
 export PGPASSWORD="${DB_PASSWORD}"
@@ -45,3 +49,6 @@ until psql -h "localhost" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q';
 DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}
 export DATABASE_URL
 sqlx database create
+sqlx migrate run
+
+>&2 echo "Postgres has been migrated, ready to go!"
