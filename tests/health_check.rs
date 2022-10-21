eadeb4c63341b19db993a4066ad545d983477026
adkelley <adkelley@mac.com>
2022-10-21 16:07:36 -0700

    Connecting to Postgres 3.8.5.3

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,12 +1,15 @@
 //! tests/health_check.rs
 
 use std::{net::TcpListener, fmt::format};
+use sqlx::{PgConnection, Connection};
+use zero2prod::startup::run;
+use zero2prod::configuration::get_configuration;
 
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0")
         .expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
-    let server = zero2prod::run(listener).expect("Failed to bind address");
+    let server = run(listener).expect("Failed to bind address");
     let _ = tokio::spawn(server);
     format!("http://127.0.0.1:{}", port)
 }
@@ -34,6 +37,11 @@ async fn subscribe_returns_a_200_for_for_valid_form_data() {
 
     // Arrange
     let app_address = spawn_app();
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let connection_string = configuration.database.connection_string();
+    let mut connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect to Posgres");
     let client = reqwest::Client::new();
 
     // Act
@@ -47,7 +55,17 @@ async fn subscribe_returns_a_200_for_for_valid_form_data() {
         .expect("Failed to execute request.");
 
     // Assert
-    assert_eq!(200, response.status().as_u16())
+    assert_eq!(200, response.status().as_u16());
+
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch saved subscription");
+
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.name, "le guin")
+
+
 }
 
 #[actix_web::test]
